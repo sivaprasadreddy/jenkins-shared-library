07c7bbf690ba33410d88d5e2496d88bf899f2d46
Siva Katamreddy <siva.katamreddy@n26.com>
2018-12-14 09:08:27 +0100

    Making reusable maven tests stage.

--- src/com/sivalabs/JenkinsJavaLib.groovy
+++ src/com/sivalabs/JenkinsJavaLib.groovy
@@ -1,12 +1,28 @@
 package com.sivalabs
 
 class JenkinsJavaLib {
+
+    // pipeline global properties
+    def steps
+    def env
+    def scm
+    def currentBuild
+
+    JenkinsJavaLib(steps, env, scm, currentBuild) {
+        this.steps = steps
+        this.env = env
+        this.scm = scm
+        this.currentBuild = currentBuild
+    }
+
     def runMavenTests() {
-        try {
-            sh './mvnw clean verify'
-        } finally {
-            junit 'target/surefire-reports/*.xml'
-            junit 'target/failsafe-reports/*.xml'
+        steps.stage("Test") {
+            try {
+                steps.sh './mvnw clean verify'
+            } finally {
+                steps.junit 'target/surefire-reports/*.xml'
+                steps.junit 'target/failsafe-reports/*.xml'
+            }
         }
     }
 }
