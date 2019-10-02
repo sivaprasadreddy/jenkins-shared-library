72bac952673b65b2570bfb6aab4b8118596c403f
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2019-10-02 07:55:35 +0530

    Adding library to be used in scripted pipeline

---
+++ src/com/sivalabs/JenkinsSharedLib.groovy
@@ -0,0 +1,34 @@
+package com.sivalabs
+
+class JenkinsSharedLib {
+
+    // pipeline global properties
+    def steps
+    def env
+    def scm
+    def currentBuild
+
+    JenkinsSharedLib(steps, env, scm, currentBuild) {
+        this.steps = steps
+        this.env = env
+        this.scm = scm
+        this.currentBuild = currentBuild
+    }
+
+    def checkout() {
+        steps.stage("Checkout") {
+            steps.checkout scm
+        }
+    }
+
+    def runMavenTests() {
+        steps.stage("Test") {
+            try {
+                steps.sh './mvnw clean verify'
+            } finally {
+                steps.junit 'target/surefire-reports/*.xml'
+                steps.junit 'target/failsafe-reports/*.xml'
+            }
+        }
+    }
+}
