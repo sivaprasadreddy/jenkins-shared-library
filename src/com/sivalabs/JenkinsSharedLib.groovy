cba4b35987e6b1d71285c5618497f328600bb2a4
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2019-10-02 08:59:01 +0530

    Adding docker image publish skeleton.

--- src/com/sivalabs/JenkinsSharedLib.groovy
+++ src/com/sivalabs/JenkinsSharedLib.groovy
@@ -5,12 +5,14 @@ class JenkinsSharedLib implements Serializable {
     // pipeline global properties
     def steps
     def env
+    def params
     def scm
     def currentBuild
 
-    JenkinsSharedLib(steps, env, scm, currentBuild) {
+    JenkinsSharedLib(steps, env, params, scm, currentBuild) {
         this.steps = steps
         this.env = env
+        this.params = params
         this.scm = scm
         this.currentBuild = currentBuild
     }
@@ -41,7 +43,7 @@ class JenkinsSharedLib implements Serializable {
     }
 
     def runOWASPChecks() {
-        steps.stage("Test") {
+        steps.stage("OWASP Checks") {
             try {
                 steps.sh './mvnw dependency-check:check'
             } finally {
@@ -53,6 +55,16 @@ class JenkinsSharedLib implements Serializable {
                         reportFiles: 'dependency-check-report.html',
                         reportName: "OWASP Dependency Check Report"
                 ])
+            }
+        }
+    }
+
+    def publishDockerImage() {
+        steps.stage("Publish Docker Image") {
+            if(params.PUBLISH_TO_DOCKERHUB) {
+                steps.echo "Publish to dockerhub. DOCKER_USERNAME=${env.DOCKER_USERNAME}, APPLICATION_NAME=${env.APPLICATION_NAME}"
+            } else {
+                steps.echo "Skipping Publish Docker Image"
             }
         }
     }
